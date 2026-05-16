6e275a61a6e20889ccc3ba7697a85ade4bac383a
rxnjitth <mranjith2506@gmail.com>
2026-05-16 16:47:22 +0530

    Database design completed :)

--- employee_management/employee/models.py
+++ employee_management/employee/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+
+class Employee(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+    department = models.CharField(max_length=100)
+    join_date = models.DateField(auto_now_add=True)
+
+    @property
+    def full_name(self):
+        return f"{self.first_name} {self.last_name}"
+
+    def __str__(self):
+        return self.full_name
+
+    class Meta:
+        ordering = ['first_name']
+        verbose_name = 'Employee'
+        verbose_name_plural = 'Employees'
